8f1994e30683a890d9b1314ec69dafd51f34f9ed
f3rnando3669 <fernandovargas3669@gmail.com>
2024-09-13 11:38:09 -0500

    uploading working and unfinshed code to convert the Adumbratio Lexici Anglico-Latini data into TEI xml

--- LatinDictApp/DictOrg.py
+++ LatinDictApp/DictOrg.py
@@ -2,13 +2,10 @@ import re
 from lxml import etree
 def DictOrg():
     text = "//    GEOGRAPHICAL NAMES // /general city: King's Mountain, Königsberg, Monterrey, Montréal  ► Regi(o)montium, i n.// /general city: Newcastle, Neuchâtel, Châteauneuf  ► Novum Castellum  ¶ 1771 WAY dedication page (of the county in Delaware).// /general city: Newport, Nieuwpoort  ► Neoportus, ûs m.  ¶ Graesse.  ► Neoportum, i n.  ¶ 1674 MILTON XIII. 28, of Belgian town."
-    titleList = []
-    DefList = []
-    restLIst = []
     title = r'/([\w\s]+):'
     Def = r':(.*)►'
     rest = r'►(.*)'
-    
+    #sepereated the given data best i can
     matches = re.findall(title, text, flags=re.IGNORECASE)
     matches2 = re.findall(Def, text, flags=re.IGNORECASE)
     matches3 = re.findall(rest, text, flags=re.IGNORECASE)
@@ -16,12 +13,15 @@ def DictOrg():
     root = etree.Element('root')
 
     # Iterate through the matches and add to XML
+    #trying to figure out the format to conv to TEI xml
     for i in matches:
         member = etree.SubElement(root, "member")
         member.attrib['title'] = i
     # Convert XML to string and print
     out = etree.tostring(root, pretty_print=True, encoding='unicode')
     print(out)
+
+
+DictOrg()
 #rescources: https://stackoverflow.com/questions/39029570/saving-list-of-tuples-in-xml
 #https://regex101.com/r/nI18g9/1
-DictOrg()
