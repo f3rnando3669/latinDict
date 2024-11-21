72218e174d9d968bb7b3bf715f561c61c4c6d9c2
fernando vargas <fernandovargas@fernandos-MacBook-Air.local>
2024-11-20 23:50:02 -0600

    Remove files

--- Computer-Science-Research-Summer-master/MachineLearningSummer/deprecated/ClientUtils/StringValidation.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def emptyString(string: str) -> bool:
-    """
-    This method checks if a string is empty\n
-    Based on content
-    """
-    return not string or bool(re.fullmatch(r'[\s]*', string=string))
